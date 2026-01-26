1746167734faa1278170989fa9a9467f95f8e68b
AdamKhelladi <khelladiadem5@gmail.com>
2026-01-26 13:03:56 +0100

    Add Navbar & Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
-
 export default function Home() {
   return (
-    <h1 className="font-bold text-2xl">Welcome to Wanderzo !!</h1>
+    <div>
+      <h1 className="font-bold text-2xl">Welcome to Wanderzo !!</h1>
+      <p>This is a travel UI/UX App for Camping</p>
+
+    </div>
   );
 }
