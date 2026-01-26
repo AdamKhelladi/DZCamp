a59dbac2fc67da2675507c29eed1f07c4847989f
AdamKhelladi <khelladiadem5@gmail.com>
2026-01-26 18:11:05 +0100

    Add logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,32 @@
+import Link from "next/link";
+import Container from "./Container/page";
+import Image from "next/image";
 
 export default function Page() {
   return (
-    <nav>
-      
-    </nav>
-  )
+    <Container>
+      <nav className="py-3 flex justify-between">
+        <Link href={"/"}>
+          <Image src="/images/wanderzoLogo.png" width={150} height={100} alt="Wanderzo Logo"/>
+        </Link>
+        <svg
+          className="w-9 h-9 mt-1 text-black cursor-pointer duration-300 hover:text-green-600"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M18 6H6m12 4H6m12 4H6m12 4H6"
+          />
+        </svg>
+      </nav>
+    </Container>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Container from "./components/Container/page";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="font-bold text-2xl">Welcome to Wanderzo !!</h1>
-      <p>This is a travel UI/UX App for Camping</p>
-
-    </div>
+    <Container>
+      <div>
+        <h1 className="font-bold text-2xl">Welcome to Wanderzo !!</h1>
+        <p>This is a travel UI/UX App for Camping</p>
+      </div>
+    </Container>
   );
 }
